cedadff61ba3349853f1a5ac41abb384e6b10e69
Pragati3011 <pragatisatpute7058@gmail.com>
2025-02-26 16:34:45 +0530

    Add files via upload

---
+++ Hibernate-annotation-based/src/main/java/com/hibernate/Main.java
@@ -0,0 +1,43 @@
+package com.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Configuration cfg =  new Configuration();
+       cfg.configure("hibranate.cfg.xml");
+
+        SessionFactory factory = cfg.buildSessionFactory();
+
+//   or     SessionFactory  factory = new Configuration().configure("hibranate.cfg.xml").buildSessionFactory();
+        Session session= factory.openSession();
+
+        Student s=new Student( 1,"pragati","ambajogai"); //seting values by constructor
+
+
+        Student  s2=new Student();
+        s2.setId(2);
+        s2.setName("Mental Pragti");
+        s2.setCity("Mental hospital");//we can set values by setter method  and constructor
+
+        Transaction transaction= session.beginTransaction();
+       // session.save(s);
+        session.save(s2);
+        transaction.commit();
+
+
+        session.close();
+       // System.out.println(factory);
+       // System.out.println(factory.isClosed());
+        factory.close();
+
+
+
+    }
+}
